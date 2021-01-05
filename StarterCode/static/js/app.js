fb1c1c546676add4b8385a5b8dddbf59cdbfc099
Benjamin Barr <benwbarr@gmail.com>
2021-01-05 15:59:52 -0700

    made function for Charts and Bar Chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,48 @@
-d3.json("samples.json").then((data)=> {
-var metadata = data.metadata;
-    console.log(metadata);
-});
+function Metadatabuild(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+
+        PANEL.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+
+    });
+}
+function Chartbuild(sample) {
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+// Bar chart
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var barData = [
+            {
+                y: yticks,
+                x: sample_values.slice(0, 10).reverse(),
+                text: otu_labels.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h",
+            }
+        ];
+
+        var barLayout = {
+            title: "Top 10 OTUs found in that individual",
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Values"},
+            margin: {t: 30, l: 150}
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+    })
+}
 
